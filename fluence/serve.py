bc083eca7886ff084c631a0ae90cb2d47e292b33
tsai phan <phanalpha@hotmail.com>
2021-11-17 16:58:32 +0800

    get balance / owner, withdraw.

--- fluence/serve.py
+++ fluence/serve.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from aiohttp import web
 from aiohttp.web_request import Request
 from decouple import config
@@ -8,24 +10,27 @@ from starkware.starknet.services.api.gateway.transaction import InvokeFunction
 from starkware.starknet.services.api.feeder_gateway.feeder_gateway_client import FeederGatewayClient
 from starkware.starkware_utils.error_handling import StarkErrorCode
 
-
-async def get_transaction(request: Request):
-    hash = request.match_info.get('hash')
-
-    feeder_client = FeederGatewayClient(
-        url=config('FEEDER_GATEWAY_URL'),
-        retry_config=RetryConfig(n_retries=1))
-    status = await feeder_client.get_transaction_status(hash)
-
-    return web.json_response(status)
+routes = web.RouteTableDef()
 
 
+def integer(x: Union[int, str]) -> int:
+    if type(x) is int:
+        return x
+
+    if x.startswith('0x'):
+        return int(x, 16)
+
+    return int(x)
+
+
+@routes.get('/api/v1/balance')
 async def get_balance(request: Request):
-    user = int(request.match_info.get('user'))
+    user = integer(request.query['user'])
+    contract = integer(request.query['contract'])
     tx = InvokeFunction(
-        contract_address=config('CONTRACT_ADDRESS', cast=lambda x: int(x, 16) if x.startswith('0x') else int(value)),
+        contract_address=config('CONTRACT_ADDRESS', cast=integer),
         entry_point_selector=get_selector_from_name('get_balance'),
-        calldata=[user],
+        calldata=[user, contract],
         signature=[])
 
     feeder_client = FeederGatewayClient(
@@ -36,32 +41,47 @@ async def get_balance(request: Request):
     return web.json_response({'balance': int(gateway_response['result'][0], 16)})
 
 
-async def post_balance(request: Request):
-    data = await request.post()
-    user = int(request.match_info.get('user'))
-    amount = int(data['amount'])
+@routes.get('/api/v1/owner')
+async def get_owner(request: Request):
+    token_id = integer(request.query['token_id'])
+    contract = integer(request.query['contract'])
     tx = InvokeFunction(
-        contract_address=config('CONTRACT_ADDRESS', cast=lambda x: int(x, 16) if x.startswith('0x') else int(value)),
-        entry_point_selector=get_selector_from_name('inc_balance' if amount > 0 else 'withdraw'),
-        calldata=[user, abs(amount)],
+        contract_address=config('CONTRACT_ADDRESS', cast=integer),
+        entry_point_selector=get_selector_from_name('get_owner'),
+        calldata=[token_id, contract],
+        signature=[])
+
+    feeder_client = FeederGatewayClient(
+        url=config('FEEDER_GATEWAY_URL'),
+        retry_config=RetryConfig(n_retries=1))
+    gateway_response = await feeder_client.call_contract(tx)
+
+    return web.json_response({'owner': gateway_response['result'][0]})
+
+
+@routes.post('/api/v1/withdraw')
+async def withdraw(request: Request):
+    data = await request.json()
+    user = integer(data['user'])
+    amount_or_token_id = integer(data['amount_or_token_id'])
+    contract = integer(data['contract'])
+    address = integer(data['address'])
+    tx = InvokeFunction(
+        contract_address=config('CONTRACT_ADDRESS', cast=integer),
+        entry_point_selector=get_selector_from_name('withdraw'),
+        calldata=[user, amount_or_token_id, contract, address],
         signature=[])
 
     gateway_client = GatewayClient(
         url=config('GATEWAY_URL'),
         retry_config=RetryConfig(n_retries=1))
     gateway_response = await gateway_client.add_transaction(tx)
-    assert gateway_response["code"] == StarkErrorCode.TRANSACTION_RECEIVED.name, \
-        f"Failed to send transaction. Response: {gateway_response}."
 
     return web.json_response({'transaction_hash': gateway_response['transaction_hash']})
 
 
 def serve():
     app = web.Application()
-    app.add_routes([
-        web.get('/tx/{hash}', get_transaction),
-        web.get('/{user}/balance', get_balance),
-        web.post('/{user}/balance', post_balance),
-    ])
+    app.add_routes(routes)
 
-    web.run_app(app)
+    web.run_app(app, port=4000)
